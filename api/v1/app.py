4ec9f9e40d1555edcce248ba00a0e206e37c0387
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-08-02 23:21:59 +0100

    feat: Set up a Flask app with blueprint routing, including teardown logic for database connection management

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from models import storage
+import os
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close_storage(exception):
+    try:
+        storage.close()
+    except Exception as e:
+        print(f"Error closing storage: {e}")
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
