94b31852283e70b8ee0d2e093b44bed1706d9328
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-08-02 23:29:13 +0100

    doc_2

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+Blueprint for API v1 routes with URL prefix '/api/v1'.
+"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Defines routes for the API v1 blueprint.
+"""
 from api.v1.views import app_views
 
 @app_views.route('/status')
