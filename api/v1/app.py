6085e304d8e9e650ef66dc8f456feba3ca6586cc
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-08-03 08:31:44 +0100

    style

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-Main application setup for the Flask API.
-"""
-from flask import Flask
-from models import storage
-import os
+"""Main application setup for the Flask API."""
 from api.v1.views import app_views
+from flask import Flask
+import os
+from models import storage
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def close_storage(exception):
