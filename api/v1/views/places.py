96dfc5276d732679e5114f4ccc0de0658c0429ed
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-08-05 14:41:17 +0100

    places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -42,7 +42,7 @@ def delete_place(place_id):
 
 @app_views.route("/cities/<city_id>/places",
                  strict_slashes=False, methods=["POST"])
-def create_city(city_id):
+def create_place(city_id):
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
@@ -56,9 +56,9 @@ def create_city(city_id):
     user = storage.get(User, data.get("user_id"))
     if user is None:
         abort(404)
-    place = Place(**data)
-    place.save()
-    return jsonify(place.to_dict()), 201
+    new_place = Place(city_id=city.id, **data)
+    new_place.save()
+    return jsonify(new_place.to_dict()), 201
 
 
 @app_views.route("/places/<place_id>",
